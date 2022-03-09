aeb93fd306f0f8e3d34f9f29960ec50b69863ba9
Майоров Михаил <m.maiorov@inbox.ru>
2022-03-09 18:01:09 +0300

    Добавьте файлы проекта.

---
+++ Classes/MaterialsPartials.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoExam5Var.Classes;
+namespace DemoExam5Var
+{
+    partial class Material
+    {
+        public string Suppliers
+        {
+            get
+            {
+                String materialSuppliers = "";
+                
+                List<Supplier> suppliers = DBConnection.materialEntities.Supplier.ToList();
+                foreach(Supplier sup in suppliers)
+                {
+                    if (sup.Material.Contains(this))
+                    {
+                        materialSuppliers += sup.Title;
+                        materialSuppliers += ", ";
+                    }
+                }
+                if(materialSuppliers == "")
+                {
+                    materialSuppliers = "Нет";
+                }
+                return materialSuppliers;
+            }
+        }
+        public string Img
+        {
+            get => Image == "" ? @"\materials\picture.png" : Image;
+        }
+    }
+}
---
+++ Pages/PgMaterialOutput.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DemoExam5Var.Classes;
+
+
+namespace DemoExam5Var
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductOutput.xaml
+    /// </summary>
+    public partial class PgMaterialOutput : Page
+    {
+        List<Material> materials = DBConnection.materialEntities.Material.ToList();
+        public PgMaterialOutput()
+        {
+            InitializeComponent();                       
+            lbMaterials.ItemsSource = materials;
+
+            cbFilter.Items.Add("Все типы");
+            foreach(var type in DBConnection.materialEntities.MaterialType)
+            {
+                cbFilter.Items.Add(type.Title);
+            }
+            cbFilter.SelectedIndex = 0;
+            cbSort.SelectedIndex = 0;
+        }
+
+        public void Filters(string materialType)
+        {
+            materials = DBConnection.materialEntities.Material.Where(x => x.MaterialType.Title == materialType).ToList(); 
+            switch (cbSort.SelectedIndex)
+            {
+                case 0:
+                    materials.OrderBy(x => x.)
+            }
+            
+            materials = 
+            lbMaterials.Items.Refresh();
+        }
+
+    }
+}
