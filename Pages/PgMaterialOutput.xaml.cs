6475def01b95315caee08d7dc91ec1f03604bd4e
Майоров Михаил <m.maiorov@inbox.ru>
2022-03-10 13:10:24 +0300

    Начал делать окно редактирования

--- Pages/PgMaterialOutput.xaml.cs
+++ Pages/PgMaterialOutput.xaml.cs
@@ -37,18 +37,71 @@ namespace DemoExam5Var
             cbSort.SelectedIndex = 0;
         }
 
-        public void Filters(string materialType)
+        public void cbChanged(object sender, SelectionChangedEventArgs e)
         {
-            materials = DBConnection.materialEntities.Material.Where(x => x.MaterialType.Title == materialType).ToList(); 
-            switch (cbSort.SelectedIndex)
-            {
-                case 0:
-                    materials.OrderBy(x => x.)
-            }
-            
-            materials = 
+            Filters();
             lbMaterials.Items.Refresh();
         }
 
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filters();
+            lbMaterials.Items.Refresh();
+        }
+        public void Filters()
+        {
+
+            if (tbSearch.Text.Length > 0)
+            {
+                materials = DBConnection.materialEntities.Material.Where(x => x.Title.Contains(tbSearch.Text)).ToList();
+            }
+
+            if (cbFilter.SelectedIndex != 0)
+            {
+                materials = materials.Where(x => x.MaterialType.Title == cbFilter.SelectedItem.ToString()).ToList();
+            }
+            else
+            {
+                materials = DBConnection.materialEntities.Material.Where(x => x.Title.Contains(tbSearch.Text)).ToList();
+            }
+            
+            switch (cbSort.SelectedIndex)
+            {
+                case 1:
+                    materials = materials.OrderBy(x => x.Title).ToList();
+                    break;
+                case 2:
+                    materials = materials.OrderByDescending(x => x.Title).ToList();
+                    break;
+                case 3:
+                    materials = materials.OrderBy(x => x.CountInStock).ToList();
+                    break;
+                case 4:
+                    materials = materials.OrderByDescending(x => x.CountInStock).ToList();
+                    break;
+                case 5:
+                    materials = materials.OrderBy(x => x.Cost).ToList();
+                    break;
+                case 6:
+                    materials = materials.OrderByDescending(x => x.Cost).ToList();
+                    break;
+                default:
+                    break;
+            }
+            lbMaterials.ItemsSource = materials;
+        }
+
+        private void lbMaterials_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(lbMaterials.SelectedIndex != -1)
+            {
+                Material item = e.AddedItems[0] as Material;
+                
+                WindMaterialRedact redact = new WindMaterialRedact(item.ID);
+                redact.ShowDialog();
+                lbMaterials.SelectedIndex = -1;
+            }
+            
+        }
     }
 }
---
+++ Windows/WindMaterialRedact.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DemoExam5Var.Classes;
+namespace DemoExam5Var
+{
+    /// <summary>
+    /// Логика взаимодействия для WindMaterialRedact.xaml
+    /// </summary>
+    public partial class WindMaterialRedact : Window
+    {
+        
+        public WindMaterialRedact(int tag)
+        {
+            InitializeComponent();
+            Material material = DBConnection.materialEntities.Material.Where(x => x.ID == tag).FirstOrDefault();
+            List<MaterialType> materialTypes = DBConnection.materialEntities.MaterialType.ToList();
+
+            List<string> typeTitle = new List<string>();
+            foreach (var type in materialTypes)
+            {
+                typeTitle.Add(type.Title);
+            }
+
+            cbMaterialType.ItemsSource = typeTitle;
+            cbMaterialType.SelectedIndex = material.MaterialTypeID;
+
+            tbTitle.Text = material.Title;
+            tbCountInPack.Text = material.CountInPack.ToString();
+            tbCountInStock.Text = material.CountInStock.ToString();
+            tbUnit.Text = material.Unit.ToString();
+            tbMinCount.Text = material.MinCount.ToString();
+            tbDescription.Text = material.Description.ToString();
+            tbCost.Text = material.Cost.ToString();
+            tbImage.Text = material.Image.ToString();
+        }
+    }
+}
